2c6a4aa444db0555dd7c2f300a6d5f9ed59a3182
sergimolina <sergimolina91@gmail.com>
2022-04-14 15:40:15 +0100

    edge correction

--- scripts/las_2_topomap/5_generate_topomap.py
+++ scripts/las_2_topomap/5_generate_topomap.py
@@ -12,10 +12,6 @@ import aerial_topomapping as at
 
 ##########################
 toponodes_filename = '../../data/riseholme_correction/riseholme_correction_toponodes_map.json'
-#datum_longitude = -0.524509505881
-#datum_latitude = 53.268642038
-#datum_longitude = 22.9243
-#datum_latitude = 40.45025
 tmap_name = "riseholme_uav_navsat"
 
 #read the toponodes file
@@ -29,12 +25,7 @@ with open("template_toponode.json", 'r') as f:
 with open("template_topoedge.json", 'r') as f:
 	template_topoedge = json.load(f)
 
-# transform the geo located nodes to the ros map coordinates  
-#corridor_toponodes_map = at.transform_toponodes_from_utm_to_map_coordinates(corridor_toponodes_utm,datum_longitude,datum_latitude)
-#corridor_toponodes_map = at.transform_toponodes_from_lonlat_to_map_coordinates_with_ros_navsat(corridor_toponodes_lonlat)
-
-
 topomap = at.generate_topological_map(corridor_toponodes_map,tmap_name,template_toponode,template_topoedge)
-#topomap = at.generate_topological_map_in_utm(corridor_toponodes_utm,tmap_name,template_toponode)
+
 with open(tmap_name+".tmap2",'w') as f:
 	yaml.dump(topomap, f)
